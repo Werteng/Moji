e676a8784abf0e076b9e9bd23d2e5ba3411fa310
Werteng <antoine.werteng@gmail.com>
2019-10-29 16:38:07 +0100

    ajout base de donnee

---
+++ Moji/Data/MojiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moji.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moji.Data
+{
+    public class MojiDbContext : DbContext
+    {
+        public DbSet<Personnage> Personnages { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=moji;Integrated Security=True;");
+        }
+    }
+}
