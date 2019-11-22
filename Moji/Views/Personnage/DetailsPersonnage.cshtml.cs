4ddcf492f279229089826c42d27c86c43358a483
BOYERAntoine <antoineb72974@gmail.com>
2019-11-22 11:43:06 +0100

    Supression Account

---
+++ Moji/Views/Personnage/DetailsPersonnage.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moji.Views.Personnage
+{
+    public class DetailsPersonnage : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
