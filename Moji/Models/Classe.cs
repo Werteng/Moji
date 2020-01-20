6eff4a252b76f9d86884319189193e89f9029957
BOYERAntoine <antoineb72974@gmail.com>
2020-01-20 16:04:42 +0100

    Ajout listes déroulantes race+classe

--- Moji/Models/Classe.cs
+++ Moji/Models/Classe.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Moji.Entities
 {
+    //[Table("Classe")]
     public class Classe
     {
         public int Id { get; set; }
--- Moji/Startup.cs
+++ Moji/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Moji.Data;
 
 namespace Moji
 {
@@ -23,7 +24,7 @@ namespace Moji
                
             });
 
-           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
